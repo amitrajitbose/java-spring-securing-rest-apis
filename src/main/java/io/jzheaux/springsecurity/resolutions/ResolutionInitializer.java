4b0455ea838fda1f98b07ad674f1b7aec043c6b6
Amitrajit_Bose@Mac <ar-amitrajit.bose@rakuten.com>
2020-07-12 18:44:19 +0530

    Working through request authentication and added test user in initializer

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -3,20 +3,23 @@ package io.jzheaux.springsecurity.resolutions;
 import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.stereotype.Component;
 
-import java.util.UUID;
-
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
-	private final ResolutionRepository resolutions;
+    private final ResolutionRepository resolutions;
+    private final UserRepository users;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
-		this.resolutions = resolutions;
-	}
+    public ResolutionInitializer(ResolutionRepository resolutions, UserRepository users) {
+        this.resolutions = resolutions;
+        this.users = users;
+    }
 
-	@Override
-	public void afterSingletonsInstantiated() {
-		this.resolutions.save(new Resolution("Read War and Peace", "user"));
-		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
-		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
-	}
+    @Override
+    public void afterSingletonsInstantiated() {
+        this.resolutions.save(new Resolution("Read War and Peace", "user"));
+        this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
+        this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+        User user1 = new User("user", "$2a$04$6Cuo9RIa1tOGrD.WsgCxGuCLoOfqChCg0dq0JVYh3R1hMgRWjgTnu");
+        this.users.save(user1);
+    }
 }
--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,13 +2,26 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
+@SpringBootApplication //(exclude = SecurityAutoConfiguration.class)
 public class ResolutionsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                org.springframework.security.core.userdetails.User
+                        .withUsername("user")
+                        .password("$2a$04$6Cuo9RIa1tOGrD.WsgCxGuCLoOfqChCg0dq0JVYh3R1hMgRWjgTnu")
+                        .authorities("resolution:read")
+                        .build());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolutionsApplication.class, args);
+    }
 
 }
