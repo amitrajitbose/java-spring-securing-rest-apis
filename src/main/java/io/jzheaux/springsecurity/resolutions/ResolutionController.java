795fdd6d1328b56e25a904369c32e5ee707fda12
Amitrajit_Bose@Mac <ar-amitrajit.bose@rakuten.com>
2020-07-14 01:08:49 +0530

    Modified health check

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -15,43 +15,59 @@ import java.util.UUID;
 
 @RestController
 public class ResolutionController {
-	private final ResolutionRepository resolutions;
+    private final ResolutionRepository resolutions;
 
-	public ResolutionController(ResolutionRepository resolutions) {
-		this.resolutions = resolutions;
-	}
+    public ResolutionController(ResolutionRepository resolutions) {
+        this.resolutions = resolutions;
+    }
 
-	@GetMapping("/resolutions")
-	public Iterable<Resolution> read() {
-		return this.resolutions.findAll();
-	}
+    @GetMapping("/resolutions")
+    public Iterable<Resolution> read() {
+        return this.resolutions.findAll();
+    }
 
-	@GetMapping("/resolution/{id}")
-	public Optional<Resolution> read(@PathVariable("id") UUID id) {
-		return this.resolutions.findById(id);
-	}
+    @GetMapping("/resolution/{id}")
+    public Optional<Resolution> read(@PathVariable("id") UUID id) {
+        return this.resolutions.findById(id);
+    }
 
-	@PostMapping("/resolution")
-	public Resolution make(@CurrentUsername String owner, @RequestBody String text) {
+    @PostMapping("/resolution")
+    public Resolution make(@CurrentUsername String owner, @RequestBody String text) {
 //		String owner = "user";
-		Resolution resolution = new Resolution(text, owner);
-		return this.resolutions.save(resolution);
-	}
+        Resolution resolution = new Resolution(text, owner);
+        return this.resolutions.save(resolution);
+    }
 
-	@PutMapping(path="/resolution/{id}/revise")
-	@Transactional
-	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
-		this.resolutions.revise(id, text);
-		return read(id);
-	}
+    @PutMapping(path = "/resolution/{id}/revise")
+    @Transactional
+    public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
+        this.resolutions.revise(id, text);
+        return read(id);
+    }
 
-	@PutMapping("/resolution/{id}/complete")
-	@Transactional
-	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
-		this.resolutions.complete(id);
-		return read(id);
-	}
+    @PutMapping("/resolution/{id}/complete")
+    @Transactional
+    public Optional<Resolution> complete(@PathVariable("id") UUID id) {
+        this.resolutions.complete(id);
+        return read(id);
+    }
 
-	@GetMapping("/health")
-	public String healthCheck(){return "App Working Successfully.";}
+    @GetMapping("/health")
+    public String healthCheck() {
+        int mb = 1024 * 1024;
+        Runtime instance = Runtime.getRuntime();
+        String responseString = "App Working Successfully\n";
+        long totalMemory = instance.totalMemory() / mb;
+        long freeMemory = instance.freeMemory() / mb;
+        long usedMemory = totalMemory - freeMemory;
+        long maxMemory = instance.maxMemory() / mb;
+        int availableProcessors = instance.availableProcessors();
+        responseString += "***** Heap utilization statistics [MB] *****\n";
+        responseString += "Total : " + totalMemory + "\n";
+        responseString += "Free : " + freeMemory + "\n";
+        responseString += "Used : " + usedMemory + "\n";
+        responseString += "Max : " + maxMemory + "\n";
+        responseString += "Available Processors: " + availableProcessors + "\n";
+        return responseString;
+    }
 }
