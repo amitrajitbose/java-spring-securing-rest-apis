cc1bac6033ccee2a5f9f0d58af858a72da03f83c
Amitrajit_Bose@Mac <ar-amitrajit.bose@rakuten.com>
2020-07-18 13:19:24 +0530

    Add admin role and user with Admin access. Add method level logic for auth

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -18,6 +18,7 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
         this.resolutions.save(new Resolution("Read War and Peace", "user"));
         this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
         this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+        this.resolutions.save(new Resolution("Read War and Peace Duplicate", "hasread"));
 
         User user = new User("user", "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
         user.grantAuthority("resolution:read");
@@ -31,5 +32,11 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
         User hasWrite = new User("haswrite", "{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
         hasWrite.grantAuthority("resolution:write");
         this.users.save(hasWrite);
+
+        User admin = new User("admin","{bcrypt}$2a$10$bTu5ilpT4YILX8dOWM/05efJnoSlX4ElNnjhNopL9aPoRyUgvXAYa");
+        admin.grantAuthority("ROLE_ADMIN");
+        admin.grantAuthority("resolution:read");
+        admin.grantAuthority("resolution:write");
+        this.users.save(admin);
     }
 }
